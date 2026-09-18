5b4ffe7d1ee3cc52e7645539a96de089b5ce4e97
kicerispy <ultratrollxd@gmail.com>
2026-09-18 02:11:59 -0500

    Update empty-plan test for startup acknowledgement

--- tests/test_task_controller.py
+++ tests/test_task_controller.py
@@ -260,5 +260,6 @@ def test_action_request_with_empty_plan_fails_without_chat_fallback():
     )
 
     assert spoken == [
+        "I'm on it. I'll keep you updated and let you know when it's finished.",
         "I couldn't create an action plan for that request.",
     ]
