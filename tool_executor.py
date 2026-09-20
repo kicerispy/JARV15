dee6510975ee7210a7033433c7c69986fb4905c1
kicerispy <ultratrollxd@gmail.com>
2026-09-20 08:21:27 -0500

    fix: include anime episodes in speech API registry

--- tool_executor.py
+++ tool_executor.py
@@ -2631,7 +2631,7 @@ def _api_spoken_summary(
     extended_tools = {
         "country_info", "crypto_price", "trivia_question", "joke",
         "meal_search", "tv_search", "music_search", "musicbrainz_search",
-        "anime_search", "ghibli_search", "openalex_search", "pubchem_lookup",
+        "anime_search", "anime_episodes", "ghibli_search", "openalex_search", "pubchem_lookup",
         "art_search", "nasa_eonet", "spacex_lookup", "sunrise_sunset",
         "topo_elevation", "public_ip", "reverse_geocode", "news_search",
         "cat_fact", "dog_image", "osm_search",
